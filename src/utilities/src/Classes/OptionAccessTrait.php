eef6e325cd163fed8f36800626b847aa21814523
Simon Asika <asika32764@gmail.com>
2019-11-09 23:21:33 +0800

    More helpers

---
+++ src/utilities/src/Classes/OptionAccessTrait.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2019 LYRASOFT.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Windwalker\Utilities\Classes;
+
+/**
+ * The OptionAccessTrait class.
+ *
+ * @since  3.0.1
+ */
+trait OptionAccessTrait
+{
+    /**
+     * Property options.
+     *
+     * @var  array|\ArrayAccess
+     */
+    protected $options = [];
+
+    /**
+     * Method to get property Options
+     *
+     * @param   string $name
+     * @param   mixed  $default
+     *
+     * @return  mixed
+     */
+    public function getOption($name, $default = null)
+    {
+        return $this->options[$name] ?? $default;
+    }
+
+    /**
+     * Method to set property options
+     *
+     * @param   string $name
+     * @param   mixed  $value
+     *
+     * @return  static  Return self to support chaining.
+     */
+    public function setOption($name, $value)
+    {
+        $this->options[$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Method to get property Options
+     *
+     * @return  array|\ArrayAccess
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Method to set property options
+     *
+     * @param   array|\ArrayAccess $options
+     *
+     * @return  static  Return self to support chaining.
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
---
+++ src/utilities/src/Classes/TraitHelper.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2019 LYRASOFT.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Windwalker\Utilities\Classes;
+
+/**
+ * The TraitHelper class.
+ *
+ * @since  3.0
+ */
+class TraitHelper
+{
+    /**
+     * classUsesRecursive
+     *
+     * @link  http://php.net/manual/en/function.class-uses.php#110752
+     *
+     * @param string|object $class
+     * @param bool          $autoload
+     *
+     * @return  array
+     */
+    public static function classUsesRecursive(string $class, bool $autoload = true): array
+    {
+        $traits = [];
+
+        do {
+            $traits[] = class_uses($class, $autoload);
+        } while ($class = get_parent_class($class));
+
+        foreach ($traits as $trait => $same) {
+            $traits[] = class_uses($trait, $autoload);
+        }
+
+        return array_unique(array_merge(...$traits));
+    }
+}
