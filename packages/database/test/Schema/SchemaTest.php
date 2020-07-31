87321bc124ff3fcf30c69faa2d67dfa37a9bb122
Simon Asika <asika32764@gmail.com>
2020-08-01 02:18:45 +0800

    Fix timestamp

--- packages/database/test/Schema/SchemaTest.php
+++ packages/database/test/Schema/SchemaTest.php
@@ -42,7 +42,7 @@ class SchemaTest extends AbstractDatabaseTestCase
         $this->instance->addPrimaryKey('id');
         $this->instance->addPrimaryKey(['foo', 'bar']);
 
-        $keys = $this->instance->getIndexes();
+        $keys = $this->instance->getConstraints();
 
         self::assertEquals(['id'], $keys['idx_ww_flower_id']->getColumns());
         self::assertEquals(['foo', 'bar'], $keys['idx_ww_flower_foo_bar']->getColumns());
